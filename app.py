d4b531704d2af68bf865942577f9a3b7f3e517a9
AnsirH <ansir0211@gmail.com>
2025-06-09 17:21:22 +0900

    fix app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
-import time
 from sommeiler import search_wine, recommand_wine, describe_dish_flavor
+import base64
 
 st.title("somelier AI")
 col1, col2 = st.columns([3, 1])
 with col1:
     uploaded_image = st.file_uploader("요리 이미지를 업로드 하세요", type=['jpg', 'jpeg', 'png'])
-    user_prompt = st.text_input("프롬프트를 입력하세요.", "예시) 이 요리에 어울리는 와인을 추천해주세요.")    
+    user_prompt = st.text_input("프롬프트를 입력하세요.", "이 요리에 어울리는 와인을 추천해주세요.")    
 
 with col2:
     if uploaded_image:
@@ -20,17 +20,24 @@ with col1:
             with st.spinner("1 단계: 요리의 맛과 향을 분석하는 중..."):
                 # 멀티 모달 모델을 이용해서 사진의 요리 특성 분석
                 # 출력
-                time.sleep(3)
                 st.markdown("### 요리의 맛과 향 분석 결과")
+                image_bytes = base64.b64encode(uploaded_image.read()).decode("utf-8")
+                dish_flavor = describe_dish_flavor(image_bytes=image_bytes, query="이 요리의 맛이나 향과 같은 특징을 한글로 분석해주세요.")
+                st.info(dish_flavor)
 
             with st.spinner("2 단계: 요리에 어울리는 와인 리뷰를 검색하는 중..."):
                 # 요리의 특성 정보로 와인을 추천하는 AI 동작
-                time.sleep(3)
                 st.markdown("### 와인 리뷰 검색 결과")
+                wine_search_result = search_wine(dish_flavor)
+                st.text(wine_search_result["wine_reviews"])
 
             with st.spinner("3 단계: AI 소리에가 와인 페어링에 대한 추천 글을 생성하는 중"):
                 # LLM을 이용해서 추천 글 생성
-                time.sleep(3)
                 st.markdown("### AI 소믈리에 와인 페어링 추천")
-                
+                wine_recommandation = recommand_wine({
+                    "dish_flavor": dish_flavor,
+                    "wine_reviews": wine_search_result["wine_reviews"]
+                })
+                st.info(wine_recommandation)
+
             st.success("추천이 완료되었습니다.")
